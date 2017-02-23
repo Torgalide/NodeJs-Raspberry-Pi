150b857bccda9002693f5aee0524a733f0a9c99a
Torgalide <torgalide@gmail.com>
2017-02-22 23:24:10 -1000

    Update to new tar format tar.xz

--- Install-Node.sh
+++ Install-Node.sh
@@ -4,13 +4,13 @@ PICHIP=$(uname -m);
 if [ "$EUID" -ne 0 ]
         then echo "You need to install as root by using sudo ./Install-Node.sh";
         exit
-else LINKTONODE=$(curl -G https://nodejs.org/dist/latest-v7.x/ | awk '{print $2}' | grep -P 'href=\"node-v7\.\d{1,}\.\d{1,}-linux-'$PICHIP'\.tar\.gz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//');
-# curl -G https://nodejs.org/dist/latest-v7.x/ | awk '{print $2}' | grep -P 'href=\"node-v7\.\d{1,}\.\d{1,}-linux-armv7l\.tar\.gz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//'
-NODEFOLDER=$(echo $LINKTONODE | sed 's/.tar.gz/\//');
+else LINKTONODE=$(curl -G https://nodejs.org/dist/latest-v7.x/ | awk '{print $2}' | grep -P 'href=\"node-v7\.\d{1,}\.\d{1,}-linux-'$PICHIP'\.tar\.xz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//');
+# curl -G https://nodejs.org/dist/latest-v7.x/ | awk '{print $2}' | grep -P 'href=\"node-v7\.\d{1,}\.\d{1,}-linux-armv7l\.tar\.xz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//'
+NODEFOLDER=$(echo $LINKTONODE | sed 's/.tar.xz/\//');
 #Shell script created by @audstanley
 #Next, Creates directory for downloads, and downloads node 7.x
 cd ~/ && mkdir tempNode && cd tempNode && wget https://nodejs.org/dist/latest-v7.x/$LINKTONODE;
-tar -xzf $LINKTONODE;
+tar -xf $LINKTONODE;
 #Remove the tar after extracing it.
 rm $LINKTONODE;
 #remove older version of node:
